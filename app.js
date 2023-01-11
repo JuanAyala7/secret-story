2401ad8049a9f3c14d2faa100dfabd74505367d0
Juan Luis Ayala Jr <113149815+JuanAyala7@users.noreply.github.com>
2023-01-10 19:09:17 -0600

    Update app.js

--- app.js
+++ app.js
@@ -1,15 +1,14 @@
 //jshint esversion:6
 require('dotenv').config();
-require('dotenv-extended').load();
 const express = require("express");
 const bodyParser = require("body-parser");
 const ejis = require("ejs");
 const mongoose = require("mongoose");
-const encrypt = require("mongoose-encryption");
+const md5 = require("md5");
 
 const app = express();
 
-console.log(process.env.API_KEY);
+console.log(md5("123456"));
 
 app.use(express.static("public"));
 app.set('view engine', 'ejs');
@@ -25,7 +24,6 @@ const userSchema = new mongoose.Schema ({
   password: String
 });
 
-userSchema.plugin(encrypt, {secret: process.env.SECRET, encryptedFields: ['password'] });
 
 const User = new mongoose.model("User", userSchema);
 
@@ -44,12 +42,21 @@ app.get("/register", function(req, res){
 app.post("/register", function(req, res){
   const newUser = new User ({
     email: req.body.username,
-    password: req.body.password
+    password: md5(req.body.password)
   });
+  newUser.save(function(err){
+    if (err) {
+      console.log(err);
+    } else {
+      res.render("secrets");
+    }
+  });
+});
+
 
 app.post("/login", function(req, res){
   const username = req.body.username;
-  const password = req.body.password;
+  const password = md5(req.body.password);
 
   User.findOne({email: username}, (err, foundUser) => {
     if (err) {
@@ -65,15 +72,6 @@ app.post("/login", function(req, res){
       } else {
         res.render("login", {errMsg: "Email or password incorrect", username: username, password: password});
       }
-    }
-  });
-});
-
-  newUser.save(function(err){
-    if (err) {
-      console.log(err);
-    } else {
-      res.render("secrets");
     }
   });
 });
